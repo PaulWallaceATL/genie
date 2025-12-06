2cdbc229ab712d046b27caedcaebedbcb76c1284
PaulWallaceATL <Paul@tryclinixai.com>
2025-12-06 00:41:54 -0500

    Add missing Link import on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { getSupabaseServerClient } from "@/lib/supabaseServer";
 import { HowItWorks } from "./components/HowItWorks";
 import { LandingHero } from "./components/LandingHero";
